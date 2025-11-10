e57a207cdbad12a8e5d73123837dec3e503355dc
Fiona Wang Github <342948522@qq.com>
2025-11-10 19:24:49 +0800

    Final clean project commit with all files

--- main.tf
+++ main.tf
@@ -298,37 +298,40 @@ resource "aws_lb_target_group_attachment" "api_tg_attach" {
 
 # Create API Gateway REST API
 resource "aws_apigatewayv2_api" "api" {
-  name          = "Project-User-Service-API"
-  protocol_type = "HTTP"
-  cors_configuration {
+    name          = "Project-User-Service-API"
+    protocol_type = "HTTP"
+    cors_configuration {
 
-    allow_origins = ["*"] 
-    allow_methods = ["*"]        
-    allow_headers = ["*"] 
-  }
+        allow_origins = ["*"] 
+        allow_methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"]        
+        allow_headers = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"]
+        expose_headers = []
+        allow_credentials = false
+        max_age = 300
+    }
 }
 
 resource "aws_apigatewayv2_integration" "api_integration" {
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "HTTP_PROXY"
-  integration_method = "ANY"
-  integration_uri    = aws_lb_listener.api_listener.arn
-  connection_type    = "VPC_LINK"
-  connection_id      = aws_apigatewayv2_vpc_link.api_vpc_link.id
-  payload_format_version = "1.0"
+    api_id             = aws_apigatewayv2_api.api.id
+    integration_type   = "HTTP_PROXY"
+    integration_method = "ANY"
+    integration_uri    = aws_lb_listener.api_listener.arn
+    connection_type    = "VPC_LINK"
+    connection_id      = aws_apigatewayv2_vpc_link.api_vpc_link.id
+    payload_format_version = "1.0"
 }
 
-resource "aws_apigatewayv2_route" "api_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "ANY /{proxy+}" 
-  target    = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
+resource "aws_apigatewayv2_route" "default_route" {
+    api_id    = aws_apigatewayv2_api.api.id
+    route_key = "$default" 
+    target    = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
 }
 
 resource "aws_apigatewayv2_stage" "api_stage" {
-  api_id      = aws_apigatewayv2_api.api.id
-  name        = "prod"
-  auto_deploy = true
-  depends_on = [ aws_apigatewayv2_route.api_route ]
+    api_id      = aws_apigatewayv2_api.api.id
+    name        = "prod"
+    auto_deploy = true
+    depends_on = [ aws_apigatewayv2_route.default_route ]
 }
 
 # RDS subnet
