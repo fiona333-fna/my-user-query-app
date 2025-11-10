f4a43f0052dce541bce3f1331e14a0133e1fc2d1
Fiona Wang Github <342948522@qq.com>
2025-11-10 15:34:16 +0800

    Final clean project commit with all files

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
+    enable_dns_support   = true
     tags = {
         Name = "my-project-vpc"
     }
@@ -38,7 +39,7 @@ resource "aws_subnet" "public_subnet" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = "10.0.1.0/24"
     availability_zone       = "ap-northeast-1a" 
-    map_public_ip_on_launch = true              # EC2启动的时候自动获得IP，与外部进行网络通信
+    map_public_ip_on_launch = true               
 
     tags = {
         Name = "public-subnet-1"
@@ -48,7 +49,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = "10.0.2.0/24"
-    availability_zone       = "ap-northeast-1a" 
+    availability_zone       = "ap-northeast-1c" 
 
     tags = {
         Name = "private-subnet-1"
@@ -58,7 +59,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "private_subnet_2" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = "10.0.3.0/24"      
-    availability_zone       = "ap-northeast-1c"  
+    availability_zone       = "ap-northeast-1d"  
     tags = { Name = "private-subnet-2" }
 }
 
@@ -82,19 +83,63 @@ resource "aws_route_table_association" "public_rt_assoc" {
     route_table_id = aws_route_table.public_rt.id
 }
 
+# Create　EIP for NAT Gateway
+resource "aws_eip" "nat" {
+    depends_on = [aws_internet_gateway.gw] 
+    tags = { Name = "Project-NAT-EIP" }
+}
+
+# NAT gateway in public subnet
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = aws_eip.nat.id
+    subnet_id     = aws_subnet.public_subnet.id  
+
+    tags = {
+        Name = "Project-NAT-Gateway"
+    }
+}
+
+# Create private route table
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "private-route-table"
+    }
+}
+
+# NAT Gateway -> Private Route Table
+resource "aws_route" "private_nat_route" {
+    route_table_id         = aws_route_table.private_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
+# Associate private route table with private subnet
+resource "aws_route_table_association" "private_rt_assoc" {
+    subnet_id      = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+# Associate private route table with private subnet2
+resource "aws_route_table_association" "private_rt_assoc_2" {
+    subnet_id      = aws_subnet.private_subnet_2.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+
 # Create Security Group for EC2 （防火墙）
 
 resource "aws_security_group" "web_sg" {
     name        = "web-security-group"
-    description = "Allow API (8080) traffic"
+    description = "Allow internal traffic from API Gateway on port 80"
     vpc_id      = aws_vpc.main.id
 
-    # Ingress rule for EC2 (Python login in 8080)
+    # Ingress rule for EC2 (Python login in 8080)   
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = 80
+        to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] 
+        cidr_blocks = [aws_vpc.main.cidr_block]  
     }
 
     # Egress rule
@@ -183,13 +228,101 @@ data "aws_ami" "amazon_linux_2" {
 resource "aws_instance" "web_server" {
     ami           = data.aws_ami.amazon_linux_2.id
     instance_type = "t3.micro" 
-    subnet_id = aws_subnet.public_subnet.id        
+    subnet_id = aws_subnet.private_subnet.id        
     vpc_security_group_ids = [aws_security_group.web_sg.id] 
     iam_instance_profile = aws_iam_instance_profile.ec2_profile.name 
 
+    associate_public_ip_address = false 
+    
+    # Nginx
+    user_data = <<-EOF
+        #!/bin/bash
+        sudo yum install nginx -y
+        echo 'server { listen 80; location / { proxy_pass http://127.0.0.1:8080; } }' | sudo tee /etc/nginx/conf.d/flask_proxy.conf
+        sudo systemctl start nginx
+        sudo systemctl enable nginx
+    EOF
+    
+    tags = { Name = "Python-API-Server" }
+}
+
+# Create VPC Link
+resource "aws_apigatewayv2_vpc_link" "api_vpc_link" {
+  name        = "project-api-vpc-link"
+  subnet_ids  = [aws_subnet.private_subnet.id, aws_subnet.private_subnet_2.id] 
+  security_group_ids = [aws_security_group.web_sg.id]
+  tags = { Name = "project-api-vpc-link" }
+}
+
+resource "aws_lb" "api_nlb" {
+    name               = "api-backend-nlb"
+    internal           = true 
+    load_balancer_type = "network" 
+    subnets            = [aws_subnet.private_subnet.id, aws_subnet.private_subnet_2.id] 
+    
     tags = {
-        Name = "Python-API-Server"
+        Name = "API-Backend-NLB"
     }
+}
+
+resource "aws_lb_target_group" "api_tg" {
+    name     = "api-nlb-tg"
+    port     = 80 
+    protocol = "TCP" 
+    vpc_id   = aws_vpc.main.id
+    health_check {
+        path = "/" 
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 30
+        timeout = 5
+    }
+}
+
+resource "aws_lb_listener" "api_listener" {
+    load_balancer_arn = aws_lb.api_nlb.arn
+    port              = 80
+    protocol          = "TCP"
+
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.api_tg.arn
+    }
+}
+
+resource "aws_lb_target_group_attachment" "api_tg_attach" {
+    target_group_arn = aws_lb_target_group.api_tg.arn
+    target_id        = aws_instance.web_server.id
+    port             = 80
+}
+
+# Create API Gateway REST API
+resource "aws_apigatewayv2_api" "api" {
+  name          = "Project-User-Service-API"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "api_integration" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "HTTP_PROXY"
+  integration_method = "ANY"
+  integration_uri    = aws_lb_listener.api_listener.arn
+  connection_type    = "VPC_LINK"
+  connection_id      = aws_apigatewayv2_vpc_link.api_vpc_link.id
+  payload_format_version = "1.0"
+}
+
+resource "aws_apigatewayv2_route" "api_route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /{proxy+}" 
+  target    = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "api_stage" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "prod"
+  auto_deploy = true
+  depends_on = [ aws_apigatewayv2_route.api_route ]
 }
 
 # RDS subnet
@@ -282,8 +415,8 @@ output "frontend_url" {
 }
 
 output "backend_api_url" {
-    description = "The Public DNS of the Python (EC2) server. Use this in React's fetch()."
-    value       = "http://${aws_instance.web_server.public_ip}:8080"
+    description = "The FINAL HTTPS endpoint URL of the API Gateway"
+    value       = aws_apigatewayv2_stage.api_stage.invoke_url
 }
 
 output "database_address" {
