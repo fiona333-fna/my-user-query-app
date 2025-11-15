6bd57dbceaab1f0175071c132fb79fb49c99a1ff
Fiona Wang Github <342948522@qq.com>
2025-11-15 15:43:32 +0800

    Final clean project commit with all files

--- main.tf
+++ main.tf
@@ -315,8 +315,6 @@ RestartSec=10
 WantedBy=multi-user.target
 EOT
 
-    sudo chown -R ec2-user:ec2-user /opt/app
-
     sudo systemctl daemon-reload
     sudo systemctl start nginx
     sudo systemctl enable nginx
